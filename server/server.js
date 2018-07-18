1521a6de4c13843a19a296582563ca38f6afbd7b
jay chacko <jaychackoui@gmail.com>
2018-07-17 23:56:55 -0500

    add patch

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ var {
 var {
     user
 } = require('./models/user');
+const _ = require('lodash');
 
 
 
@@ -94,6 +95,33 @@ app.delete('/todos/:id', (req, res) => {
 
 
 })
+
+
+app.patch('/todos/:id', (req,res) => {
+    var id = req.params.id;;
+    var body = _.pick(req.body, ['text', 'completed']);
+     console.log('body: ', body);
+    
+     if (!ObjectID.isValid(id)) {
+       return res.status(404).send();
+     }
+     if (_.isBoolean(body.completed) && body.completed) {
+       body.completedAt = new Date().getTime();
+     } else {
+       body.completed = false;
+       body.completedAt = null;
+     }
+     Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+       if (!todo) {
+         return res.status(400).send();
+       } 
+    
+       res.send({todo});
+     })
+    .catch((err) => {
+      res.status(400).send();
+    });
+   });
 
 app.listen(Port, () => {
     console.log(`Port opened at ${Port}`)
